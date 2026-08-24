deb93deec7dfb3ef689b62d6067d946f3f5597ee
Abhijeet Darekar <abhijeetdarekar3699@gmail.com>
2026-08-24 18:59:45 +0530

    feat: add spring boot application

---
+++ backend/src/main/java/com/abhijeet/cognodb/CognodbHealthcareApplication.java
@@ -0,0 +1,12 @@
+package com.abhijeet.cognodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognodbHealthcareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CognodbHealthcareApplication.class, args);
+    }
+}
