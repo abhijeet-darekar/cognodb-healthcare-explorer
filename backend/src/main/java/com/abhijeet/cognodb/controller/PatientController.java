932d3f9ac20320d0d0ba3f82d4ea89ad3cc66d99
Abhijeet Darekar <abhijeetdarekar3699@gmail.com>
2026-09-01 22:42:21 +0530

    Fix patient API CORS

--- backend/src/main/java/com/abhijeet/cognodb/controller/PatientController.java
+++ backend/src/main/java/com/abhijeet/cognodb/controller/PatientController.java
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:5175",
+        "https://cognodb-healthcare-explorer-1.onrender.com"
+})
 @RestController
 @RequestMapping("/patients")
 public class PatientController {
